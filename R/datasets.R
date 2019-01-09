c6c995ef096b0814ef5dd3f33ce254ac7047333d
Tiago Lubiana <tiagolubiana@hotmail.com>
2019-01-09 17:07:49 -0200

    first commit

---
+++ R/datasets.R
@@ -0,0 +1,9 @@
+#' PanglaoDB of gene markers
+#'
+#' database of gene markers downloaded from https://panglaodb.se/markers.html?cell_type=%27all_cells%27#
+#' @name panglaoDB
+#' @docType data
+#' @usage data(panglaodb)
+#' @format data frame
+#' @keywords datasets, cell-markers, database
+"panglaoDB"
---
+++ R/wcit.R
@@ -0,0 +1,29 @@
+#'@import dplyr
+NULL
+
+#' A "which cell is that" function.
+#'
+#' This function allows you to infer the cell identity from a list of putative markers
+#' @param markers A vector containing the gene markers in Gene Symbol format, for humans
+#' @param panglao A boolean determining if panglaoDB is going to be used or not. Defaults to TRUE
+#' @keywords single-cell
+#' @export
+#' @examples
+#' set.seed(3)
+#' data(panglaodb)
+#' markers <- panglaodb$official.gene.symbol[c(1:10, sample(x = 1:7000,size = 10))]
+#' wcit(markers)
+
+
+wcit <- function(markers, panglao = T){
+
+  marker_hits <- filter(panglaodb, official.gene.symbol %in% markers)
+
+  cell_rank <- table(marker_hits$cell.type)
+
+  cell_rank <- sort(cell_rank, decreasing = T)
+
+  head(cell_rank)
+
+
+}
