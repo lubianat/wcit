6094791b6ef3bab3ec5f989134e8a6625c17faf4
Tiago Lubiana <tiagolubiana@hotmail.com>
2019-04-06 19:02:53 -0300

    Adding integration to Seurat's framework

---
+++ R/wcit_magic.R
@@ -0,0 +1,32 @@
+
+#' An integration of the wcit package to the Seurat framework
+#'
+#' This function allows you to infer the Seurat clusters identity from a list of putative markers
+#'
+#' @param seurat_object A seurat object already clusterized.
+#' @param number_of_markers Number of markers extracted from the FindAllMarkers
+#' @import Seurat
+#' function that will be used for calculation. Defaults to 10.
+#' @keywords single-cell
+#' @export
+
+wcit_magic <- function (seurat_object, number_of_markers = 10 ){
+
+  markers <- Seurat::FindAllMarkers(seurat_object)
+  les_marcadeurs <- markers %>% group_by(cluster) %>% top_n(number_of_markers, avg_logFC)
+
+  guesses <- c()
+
+  for (i in levels(les_marcadeurs$cluster)){
+
+    selected_markers <- les_marcadeurs$gene[les_marcadeurs$cluster==i]
+    wcit_result <-wcit(selected_markers)
+    guesses <- c(guesses, names(wcit_result[['panglaoDB']][1]))
+  }
+
+  current.cluster.ids <- levels(seurat_object@ident)
+  new.cluster.ids <- guesses
+  seurat_object@ident <- plyr::mapvalues(x = seurat_object@ident, from = current.cluster.ids, to = new.cluster.ids)
+  TSNEPlot(object = seurat_object, do.label = TRUE, pt.size = 0.5)
+  return(seurat_object)
+}
