f37fe59b16c82704443d2ee8cca6a301abe511cc
Tiago Lubiana <tiago.lubiana.alves@usp.br>
2019-07-09 19:03:30 -0300

    updating panglao db

--- R/wcit.R
+++ R/wcit.R
@@ -7,19 +7,19 @@
 #' @keywords single-cell
 #' @export
 #' @examples
-#' set.seed(3)
+#'   set.seed(3)
 #'
-#' data(panglaoDB)
-#' markers <- panglaodb$official.gene.symbol[c(1:10, sample(x = 1:7000,size = 10))]
-#' wcit(markers)
+#'   data(panglaoDB)
+#'   markers <- panglaoDB$`official gene symbol`[c(1:10, sample(x = 1:7000,size = 10))]
+#'   wcit(markers)
 
 
 wcit <- function(markers, panglao = T, CellMarkers = T ){
   ranks <- list()
   if (panglao == T){
     data("panglaoDB")
-    marker_hits <- panglaoDB[panglaoDB$official.gene.symbol %in% markers,]
-    cell_rank <- table(marker_hits$cell.type)
+    marker_hits <- panglaoDB[panglaoDB$`official gene symbol` %in% markers,]
+    cell_rank <- table(marker_hits$`cell type`)
     cell_rank <- sort(cell_rank, decreasing = T)
     ranks$panglaoDB <-cell_rank
     print(paste("Top cell types according to", 'panglaoDB', ':'))
